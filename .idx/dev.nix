355eb474c74817e0c09a276cdf68c07699dc518f
blimacardoso <blimacardoso@gmail.com>
2025-06-28 16:13:39 +0000

    build(nix): it setup the nix environment to run the system at the firebase studio

---
+++ .idx/dev.nix
@@ -0,0 +1,61 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    pkgs.python3Full
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.virtualenv
+    pkgs.python3Packages.pdm-backend
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+    pkgs.pdm
+    pkgs.pipx
+    pkgs.commitizen
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and show it in IDX's web preview panel
+          command = ["pdm" "run" "serve"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            CHAINLIT_PORT = "$PORT";
+            DEFAULT_PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        pdm-init = "pdm init";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        pdm-install = "pdm install";
+      };
+    };
+  };
+}
